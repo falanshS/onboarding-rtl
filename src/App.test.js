4a64e9ab07f47f3c4bb8271e14b379b1ca80f454
Anuroop R <anuroop.r@capillarytech.com>
2023-07-06 16:40:40 +0530

    added chapter 3 test

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,48 @@ import App from './App';
 describe('Tests for Todo App',()=>{
     // Write tests for Todo App
     // Add and  Delete Todo
-    it('', () => {
-      })
+    it('should create a todo', async() => {
+      render(<App/>)
+      const todoField=screen.getByRole('textbox');
+      expect(todoField).toBeInTheDocument();
+      await userEvent.type(todoField,"todo test");
+
+      const createBtn=screen.getByRole('button', {
+        name: /create/i
+      });
+      expect(createBtn).toBeInTheDocument();
+      userEvent.click(createBtn);
+
+      const newTodo=screen.getByTestId("todo").querySelector('span');
+      // console.log(newTodo)
+      expect(newTodo).toBeInTheDocument();
+      expect(newTodo.textContent).toBe("todo test")
+
+    })
+
+    it('should delete a created todo', async() => {
+      render(<App/>)
+      const todoField=screen.getByRole('textbox');
+      expect(todoField).toBeInTheDocument();
+      await userEvent.type(todoField,"todo test");
+
+      const createBtn=screen.getByRole('button', {
+        name: /create/i
+      });
+      expect(createBtn).toBeInTheDocument();
+      userEvent.click(createBtn);
+
+      const newTodo=screen.getByTestId("todo").querySelector('span');
+      // console.log(newTodo)
+      expect(newTodo).toBeInTheDocument();
+      expect(newTodo.textContent).toBe("todo test")
+      // screen.debug();
+      const deleteBtn=screen.getByTestId("deleteButton");
+      expect(deleteBtn).toBeInTheDocument();
+      userEvent.click(deleteBtn);
+      // screen.debug();
+      expect(newTodo).not.toBeInTheDocument()
+      
+    })
+
 })
